4a1cb9d59202b2317229c6b572ddf30a8118157c
finashin82 <finashin82@mail.ru>
2025-10-05 20:59:07 +0900

    Почти сделал зомби с анимациями

---
+++ The Zombie Hunter/Assets/Scripts/UtilityAISystem/StateController.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    // Список с состояниями
+    [SerializeField] private List<State> _state;
+
+    private State stateCurrent = null;
+   
+    void Update()
+    {        
+        float maxRadius = 0;
+
+        foreach (var state in _state) 
+        {
+            var radius = state.Evaluate();
+
+            if (maxRadius < radius)
+            {
+                // При смене состояния нужно, сначала, завершить все в текущем состоянии (например анимации)
+                if (stateCurrent != null) stateCurrent.Exit();
+
+                maxRadius = radius;
+
+                stateCurrent = state;
+            }
+        }
+
+        if (stateCurrent != null) 
+        { 
+            stateCurrent.Execute();  
+        }        
+    }
+}
+
